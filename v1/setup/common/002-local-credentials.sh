ebd7c7fa780d6ce4ff10f8ce0de3dc798c5680b3
Matt Fuller <fuller@adobe.com>
2016-05-27 16:24:08 -0400

    running scripts with sudo and hard coding core where needed

--- v1/setup/common/002-local-credentials.sh
+++ v1/setup/common/002-local-credentials.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/bash -x
 
-HOMEDIR=$(eval echo "~`whoami`")
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-OWNER=$(whoami)
 
 source /etc/environment
 source $DIR/../../lib/helpers.sh
@@ -12,21 +10,21 @@ echo "-------Beginning local credentials setup-------"
 # Get the docker config from etcd
 DOCKERCFG_CONTENTS=$(etcd-get /docker/config/dockercfg)
 
-echo "$DOCKERCFG_CONTENTS" > /home/${OWNER}/.dockercfg
-sudo chown -R ${OWNER}:${OWNER} /home/${OWNER}/.dockercfg
-sudo cp /home/${OWNER}/.dockercfg /root/.dockercfg
+echo "$DOCKERCFG_CONTENTS" > /home/core/.dockercfg
+sudo chown -R core:core /home/core/.dockercfg
+sudo cp /home/core/.dockercfg /root/.dockercfg
 
 # Actually generate a private key
-if [[ ! -f ${HOMEDIR}/.ssh/id_rsa ]]; then
-	ssh-keygen -f ${HOMEDIR}/.ssh/id_rsa -t rsa -N ''
+if [[ ! -f /home/core/.ssh/id_rsa ]]; then
+	ssh-keygen -f /home/core/.ssh/id_rsa -t rsa -N ''
 fi
 
 # ensure that we have a public key for our RSA key and that it's authorized
-ssh-keygen -f ${HOMEDIR}/.ssh/id_rsa -y > ${HOMEDIR}/.ssh/id_rsa.pub
-cat ${HOMEDIR}/.ssh/id_rsa.pub >> ${HOMEDIR}/.ssh/authorized_keys
+ssh-keygen -f /home/core/.ssh/id_rsa -y > /home/core/.ssh/id_rsa.pub
+cat /home/core/.ssh/id_rsa.pub >> /home/core/.ssh/authorized_keys
 
 # ignore requests against github.com
 # TODO: maybe...re-evaluate this
-echo -e "Host github.com\n\tStrictHostKeyChecking no\n" > ${HOMEDIR}/.ssh/config
+echo -e "Host github.com\n\tStrictHostKeyChecking no\n" > /home/core/.ssh/config
 
 echo "-------Done local credentials setup-------"
