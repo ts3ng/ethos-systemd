b915a1547a96ea11fbbb679e4bcd7475ffbd2403
Matt Fuller <matthewdf10@gmail.com>
2016-07-14 19:50:45 -0400

    Adding Aqua Service (#22)
    * adding aqua service units
    
    * configuring aqua service with rds and internal elb for agent gateway communication
    
    * adding script to write current aqua gateway to etcd
    
    * adding restart to ecr, removing link for gateway
    
    * adding wait condition for ecr login
    
    * waiting longer for iam proxy
    
    * fixing ecr loop
    
    * adding wait script for aqua agent and console
    
    * adding restart on failure for aqua
    
    * removing timeout for aqua
    
    * removing aqua detached mode

--- v1/setup/leader/003-optional.sh
+++ v1/setup/leader/003-optional.sh
@@ -2,8 +2,24 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR/../../lib/helpers.sh
+source /etc/environment
 
 echo "-------Leader node, beginning optional setup scripts-------"
+
+IMAGE=$(etcd-get /images/secrets-downloader)
+docker pull $IMAGE
+
+SERVICES_OVERRIDE=$(sudo docker run --rm behance/docker-aws-secrets-downloader --table $SECRETS_TABLE --key configs | grep /environment/services)
+
+if [[ ! -z $SERVICES_OVERRIDE ]]; then
+	SERVICES=$(sudo docker run --rm behance/docker-aws-secrets-downloader --table $SECRETS_TABLE --key configs --name /environment/services)
+
+	CONFIG_PATH=`echo $SERVICES | cut -d' ' -f1`
+	CONFIG_VAL=`echo $SERVICES | cut -d' ' -f2-`
+
+	etcd-set -- $CONFIG_PATH "$CONFIG_VAL"
+fi
+
 for service in $(etcd-get /environment/services)
 do
   servicedir=$DIR/../../opt/${service}/setup/leader
