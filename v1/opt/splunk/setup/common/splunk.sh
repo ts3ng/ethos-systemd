269a3e9cd70acc6692524ccf325cb692e1044420
ts3ng <tseng@adobe.com>
2016-08-11 15:08:52 -0700

    issue 850 splunk changes (#53)
    * changes to support splunk as a service
    
    * adding new field and data cloning
    
    * quotes around stackname
    
    * splunk changes
    
    * consolidating pipe services
    
    * removing old services
    
    * splunkssl changes in inputs file
    
    * changing extension for cloudops cert
    
    * adding cert format options
    
    * removing redudndant STACK_NAME setting

--- v1/opt/splunk/setup/common/splunk.sh
+++ v1/opt/splunk/setup/common/splunk.sh
@@ -12,29 +12,35 @@ SPLUNK_CLOUDOPS_SSLPASSWORD=$(etcd-get /splunk/config/cloudops-sslpassword)
 SPLUNK_SECOPS_INDEX=$(etcd-get /splunk/config/secops-index)
 SPLUNK_CLOUDOPS_INDEX=$(etcd-get /splunk/config/cloudops-index)
 SPLUNK_FORWARDER_HOST=`curl -s http://169.254.169.254/latest/meta-data/hostname`
+SPLUNK_CLOUDOPS_CERTPATH_FORMAT=$(etcd-get /splunk/config/cloudops-certpath-format)
+SPLUNK_SECOPS_CERTPATH_FORMAT=$(etcd-get /splunk/config/secops-certpath-format)
+SPLUNK_CLOUDOPS_ROOTCA_FORMAT=$(etcd-get /splunk/config/cloudops-rootca-format)
+SPLUNK_SECOPS_ROOTCA_FORMAT=$(etcd-get /splunk/config/secops-rootca-format)
 
 
 #create splunk configuration directory
 mkdir -p $SPLUNK_DIR
 #generate/fetch secure certs in configurations directory
-cat << EOF > /$SPLUNK_DIR/secopsCA.crt
+cat << EOF > /$SPLUNK_DIR/secopsCA.$SPLUNK_CLOUDOPS_CERTPATH_FORMAT
 $(etcd-get /splunk/config/secopsca-cert | awk '{gsub(/\\n/,"\n")}1')
 EOF
 
-cat << EOF > /$SPLUNK_DIR/secopsForwarder.pem
+cat << EOF > /$SPLUNK_DIR/secopsForwarder.$SPLUNK_CLOUDOPS_ROOTCA_FORMAT
 $(etcd-get /splunk/config/secopsforwarder-cert | awk '{gsub(/\\n/,"\n")}1')
 EOF
 
 
-cat << EOF > /$SPLUNK_DIR/cloudopsCA.crt
+cat << EOF > /$SPLUNK_DIR/cloudopsCA.$SPLUNK_CLOUDOPS_CERTPATH_FORMAT
 $(etcd-get /splunk/config/cloudopsca-cert | awk '{gsub(/\\n/,"\n")}1')
 EOF
 
-cat << EOF > /$SPLUNK_DIR/cloudopsForwarder.pem
+cat << EOF > /$SPLUNK_DIR/cloudopsForwarder.$SPLUNK_CLOUDOPS_ROOTCA_FORMAT
 $(etcd-get /splunk/config/cloudopsforwarder-cert | awk '{gsub(/\\n/,"\n")}1')
 EOF
+
 #set default groups, default to genericForwarder if cloudops and secops enabled then set if cloudops enabled onyl.
 DEFAULTGROUP="splunkssl-genericForwarder"
+
 if [ "$SPLUNK_ENABLE_ClOUDOPS_FORWARDER" == "1" ]; then
   DEFAULTGROUP="splunkssl-secondaryForwarder"
 fi
@@ -64,8 +70,8 @@ cat << EOF >> /$SPLUNK_DIR/outputs.conf
 
 [tcpout:splunkssl-genericForwarder]
 server = $SPLUNK_FORWARD_SECOPS_SERVER_LIST
-sslCertPath = /opt/splunk/etc/system/local/secopsForwarder.pem
-sslRootCAPath = /opt/splunk/etc/system/local/secopsCA.crt
+sslCertPath = /opt/splunk/etc/system/local/secopsForwarder.$SPLUNK_SECOPS_CERTPATH_FORMAT
+sslRootCAPath = /opt/splunk/etc/system/local/secopsCA.$SPLUNK_SECOPS_ROOTCA_FORMAT
 sslPassword = $SPLUNK_SECOPS_SSLPASSWORD
 sslVerifyServerCert = false
 EOF
@@ -81,10 +87,10 @@ fi
 if [ "$SPLUNK_ENABLE_CLOUDOPS_FORWARDER" == "1" ]; then
 cat << EOF >> /$SPLUNK_DIR/outputs.conf
 
-[tcpout:splunk-secondaryForwarder]
+[tcpout:splunkssl-secondaryForwarder]
 server = $SPLUNK_FORWARD_CLOUDOPS_SERVER_LIST
-sslCertPath = /opt/splunk/etc/system/local/cloudopsForwarder.pem
-sslRootCAPath = /opt/splunk/etc/system/local/cloudopsCA.crt
+sslCertPath = /opt/splunk/etc/system/local/cloudopsForwarder.$SPLUNK_CLOUDOPS_CERTPATH_FORMAT
+sslRootCAPath = /opt/splunk/etc/system/local/cloudopsCA.$SPLUNK_CLOUDOPS_ROOTCA_FORMAT
 sslPassword = $SPLUNK_CLOUDOPS_SSLPASSWORD
 sslVerifyServerCert = false
 EOF
