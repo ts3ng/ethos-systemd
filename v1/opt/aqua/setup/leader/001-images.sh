946fe4f8f70a36415c5e3f11ba568bf9122ee5d3
Anshu Pande <anshu.pande@gmail.com>
2016-09-12 15:24:46 -0400

    workaround for aqua (#73)
    * workaround for aqua
    
    * add DOCKER_SCHEDULER=fleet to agent service
    
    * add DOCKER_SCHEDULER=fleet to agent service docker run options
    
    * add missing - to the options
    
    * add an option RESTART_CONTAINERS=no
    
    * updated aqua agent with latest 1.2.1
    
    * add user core

--- v1/opt/aqua/setup/leader/001-images.sh
+++ v1/opt/aqua/setup/leader/001-images.sh
@@ -4,5 +4,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR/../../../../lib/helpers.sh
 
 etcd-set /images/scalock-gateway    "index.docker.io/aquasec/gateway:1.2"
-etcd-set /images/scalock-agent      "index.docker.io/aquasec/agent:1.2"
+etcd-set /images/scalock-agent      "index.docker.io/aquasec/agent:1.2.1"
 etcd-set /images/scalock-server     "index.docker.io/aquasec/server:1.2"
