c523710c837cf72b8ac5e7456d5235efcbc94534
eadasiak <eadasiak@adobe.com>
2016-10-24 13:44:55 -0700

    Updating after Mesosphere modified their tags

--- v1/setup/leader/001-defaults.sh
+++ v1/setup/leader/001-defaults.sh
@@ -19,7 +19,7 @@ etcd-set /images/klam-ssh               "index.docker.io/behance/klam-ssh:v1"
 etcd-set /images/chronos                "index.docker.io/mesosphere/chronos:chronos-2.5.0-0.1.20160824153434.ubuntu1404-mesos-1.0.0"
 etcd-set /images/flight-director        "index.docker.io/behance/flight-director:8e05dddcb45f70258de4400d22fcbab9afb830f0"
 etcd-set /images/marathon               "index.docker.io/mesosphere/marathon:v1.3.0"
-etcd-set /images/mesos-master           "index.docker.io/mesosphere/mesos-master:1.0.11.0.1-2.0.93.ubuntu1404"
+etcd-set /images/mesos-master           "index.docker.io/mesosphere/mesos-master:1.0.1-2.0.93.ubuntu1404"
 etcd-set /images/zk-exhibitor           "index.docker.io/behance/docker-zk-exhibitor:v1.0.0"
 etcd-set /images/cfn-signal             "index.docker.io/behance/docker-cfn-bootstrap:v1.0.0"
 etcd-set /images/jenkins                "index.docker.io/jenkins:1.651.1"
@@ -30,7 +30,7 @@ etcd-set /images/capcom                 "index.docker.io/behance/capcom:3ddcfe36
 etcd-set /images/proxy                  "index.docker.io/nginx:1.9.5"
 etcd-set /images/control-proxy          "index.docker.io/behance/apigateway:v0.0.2"
 
-etcd-set /images/mesos-slave            "index.docker.io/mesosphere/mesos-slave:1.0.11.0.1-2.0.93.ubuntu1404"
+etcd-set /images/mesos-slave            "index.docker.io/mesosphere/mesos-slave:1.0.1-2.0.93.ubuntu1404"
 
 etcd-set /images/etcd-locks              "index.docker.io/adobeplatform/etcd-locks:v0.1"
 
