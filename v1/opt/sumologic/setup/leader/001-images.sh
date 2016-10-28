4a049364cbdeff017fac6ba4589ee59ce07b704d
Malcolm Jones <malcolm@adobe.com>
2016-10-28 15:31:27 -0400

    chg: setting back image tags sumologic

--- v1/opt/sumologic/setup/leader/001-images.sh
+++ v1/opt/sumologic/setup/leader/001-images.sh
@@ -3,5 +3,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR/../../../../lib/helpers.sh
 
-etcd-set /images/sumologic              "index.docker.io/behance/docker-sumologic:ko-mask"
-etcd-set /images/sumologic-syslog       "index.docker.io/behance/docker-sumologic:ko-mask-syslog"
+etcd-set /images/sumologic              "index.docker.io/behance/docker-sumologic:latest"
+etcd-set /images/sumologic-syslog       "index.docker.io/behance/docker-sumologic:syslog-latest"
