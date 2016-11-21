0a16f99d714430912459941446cedfda72051773
Matt Fuller <fuller@adobe.com>
2016-11-21 10:08:48 -0500

    User Namespaces (#46)
    * adding framework auth to newer mesos and marathon
    
    * fixing creds format for chronos and marathon
    
    * fixing mesos and chronos auth secret file
    
    * enabling user namespaces
    
    * fixing cli opts for mesos and zk in namespaces
    
    * putting back
    
    * removing zk fixes
    
    * turning on custom zk changes for namespaces
    
    * booster privilege fix, zk update to non-root container
    
    * updating zk permissions of cfg file
    
    * using new container for zk namespaces
    
    * adding more ns commands to fleet units
    
    * undoing auth fixes to separate prs
    
    * removing mesos auth updates

--- v1/setup/leader/001-defaults.sh
+++ v1/setup/leader/001-defaults.sh
@@ -20,7 +20,7 @@ etcd-set /images/chronos                "index.docker.io/mesosphere/chronos:chro
 etcd-set /images/flight-director        "index.docker.io/behance/flight-director:697d304cf2cbd0b6d2c10179292d7c73664470d6"
 etcd-set /images/marathon               "index.docker.io/mesosphere/marathon:v1.3.0"
 etcd-set /images/mesos-master           "index.docker.io/mesosphere/mesos-master:1.0.1-2.0.93.ubuntu1404"
-etcd-set /images/zk-exhibitor           "index.docker.io/behance/docker-zk-exhibitor:v1.0.0"
+etcd-set /images/zk-exhibitor           "index.docker.io/behance/docker-zk-exhibitor:ns1.5"
 etcd-set /images/cfn-signal             "index.docker.io/behance/docker-cfn-bootstrap:v1.0.0"
 etcd-set /images/jenkins                "index.docker.io/jenkins:1.651.1"
 etcd-set /images/booster                "index.docker.io/behance/booster:0.5"
